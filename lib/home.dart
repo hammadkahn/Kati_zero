f5c5bcf34b9a2625bd7a6d7b37fa6d58868f12ce
hammadkahn <hammad.khan0312@gmail.com>
2022-03-18 00:03:30 +0500

    second screen

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -6,7 +7,39 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          const Padding(padding: EdgeInsets.only(top: 71)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              SvgPicture.asset(
+                'images/zeroo.svg',
+                width: 91,
+                height: 91,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 28),
+                child: Text(
+                  "VS",
+                  style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xFFDBDBDB)),
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(left: 25)),
+              SvgPicture.asset(
+                'images/cross.svg',
+                width: 91,
+                height: 91,
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
