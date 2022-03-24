7cad5176d34824bb299f214cdc122f98867cc707
hammadkahn <hammad.khan0312@gmail.com>
2022-03-24 23:39:48 +0500

    ui's 1

--- lib/leaderboard.dart
+++ lib/leaderboard.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:myapp/winbox_1.dart';
+import 'package:myapp/winbox_2.dart';
 
 class LeaderBoard extends StatelessWidget {
   const LeaderBoard({Key? key}) : super(key: key);
@@ -10,26 +12,35 @@ class LeaderBoard extends StatelessWidget {
       backgroundColor: Colors.white,
       body: Column(
         children: [
-          Row(
-            children: [
-              SvgPicture.asset('images/return.svg'),
-              Column(children: [
-                // Padding(padding: EdgeInsets.only(left: 36)),
-                Text(
-                  "L E A D E R",
-                  style: TextStyle(
-                      color: Color(0xFF0D47A1),
-                      fontSize: 25,
-                      fontWeight: FontWeight.w700),
+          Padding(
+            padding: EdgeInsets.only(left: 32, top: 60),
+            child: Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SvgPicture.asset('images/return.svg'),
+                Padding(
+                  padding: const EdgeInsets.only(left: 36),
+                  child: Column(children: [
+                    Text(
+                      "L E A D E R",
+                      style: TextStyle(
+                          color: Color(0xFF0D47A1),
+                          fontSize: 25,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    Text("B O A R D",
+                        style: TextStyle(
+                            color: Color(0xFF0D47A1),
+                            fontSize: 35,
+                            fontWeight: FontWeight.w700))
+                  ]),
                 ),
-                Text("B O A R D",
-                    style: TextStyle(
-                        color: Color(0xFF0D47A1),
-                        fontSize: 35,
-                        fontWeight: FontWeight.w700))
-              ]),
-            ],
+              ],
+            ),
           ),
+          Padding(padding: EdgeInsets.only(top: 56)),
+          WinBox_2(),
+          WinBox_1()
         ],
       ),
     );
